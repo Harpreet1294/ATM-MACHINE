53abcc73684bbcff279f8087e4daa50816641530
Harpreet1294 <harpreetkaur750805@gmail.com>
2020-08-21 22:19:12 +0530

    clear_screen and read_file

--- ATM PROJECT.py
+++ ATM PROJECT.py
@@ -17,3 +17,17 @@ try:
 except FileNotFoundError:
     f = open('Accounts.txt', 'w')
     f.close()
+'-----------------------------------------------'
+import os
+import time
+def clear_screen()
+os.system('clear')
+print()
+def read_file(file_name):
+    opened_file=open(file_name,'r')
+    lines_list=[]
+    for line in opened_file:
+        line=line.split()
+        lines_list.append(line)
+    return lines_list
+
