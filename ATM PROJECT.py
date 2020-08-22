e0519f815fb29924d09825474ab698a840e20d59
Harpreet1294 <harpreetkaur750805@gmail.com>
2020-08-22 10:09:54 +0530

    create_account

--- ATM PROJECT.py
+++ ATM PROJECT.py
@@ -121,4 +121,25 @@ def def show_history(ls):
                 for elements in acc:
                     acc_file.write("%s\t" % elements )
                 acc_file.write('\n')
+    def create_account(ls):
+        account_name = input('Enter Your Name (WITHOUT SPACES): ')
+        account_password = input('Enter Your Password (WITHOUT SPACES): ')
+        print("Creating Your Account .....")
+        accounts_file = open('Accounts.txt', 'a')
+        if len(ls)==0:
+            new_last_id=1
+        else:
+            new_last_id=int(ls[len(ls) - 1][0]) + 1
+        line='{0}\t{1}\t{2}\t0\n'
+        accounts_file.write(line)
+        id_file_name = str(new_last_id) + '.txt'
+        id_file = open(id_file_name, 'w')
+
+        print("Your Account Has Been Created And Your Id Is " + str(new_last_id))
+        id_file.close()
+        accounts_file.close()
+        ls.append([str(new_last_id), account_name, account_password, '0'])
+
+
+
 
