61df164de999e2a48281f09724e1224b4731518e
Harpreet1294 <harpreetkaur750805@gmail.com>
2020-08-22 09:58:20 +0530

    login

--- ATM PROJECT.py
+++ ATM PROJECT.py
@@ -99,3 +99,26 @@ def def show_history(ls):
 
         input('\nPress Enter to go back..')
         os.system('clear')
+    def login(acc_list):
+        login_id=input('Please, Enter your info(press "Ctrl+C" to go back) \n>>ID: ')
+        login_password=intput('>>password: ')
+        found=False
+        for account in acc_list:
+            if account[0]==login_id and account[2]==login_password:
+                found=True
+                menu2(account)
+                break
+            else:
+                continue
+        if not found:
+            clear_screen()
+            print('Wrong ID or Password')
+            login(acc_list)
+        else:
+            acc_file=open('Accounts.txt','w')
+            print('Saving changes...')
+            for acc in acc_list:
+                for elements in acc:
+                    acc_file.write("%s\t" % elements )
+                acc_file.write('\n')
+
